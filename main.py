930e2252f91b04220959c041d198cb8727b8ef4b
Sibints2000 <sibints2000@gmail.com>
2021-08-24 18:48:48 +0530

    Fiftieth Commit

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def count_down(count):
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
-count_down(5)
+
 
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35))
 title_label.grid(column=1, row=0)
@@ -34,6 +34,8 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
+
+count_down(5)
 
 start_button = Button(text="Start", highlightthickness=0)
 start_button.grid(column=0, row=2)
