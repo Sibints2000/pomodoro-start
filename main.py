9ffd5afb03f43f1521c4d7abdf4fb3cef1a1394d
Sibints2000 <sibints2000@gmail.com>
2021-08-24 19:33:30 +0530

    Eighty First Commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ timer = None
 
 
 def reset_timer():
-    window.after_cancel()
+    window.after_cancel(timer)
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
@@ -79,7 +79,7 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(fg=GREEN, bg=YELLOW)
