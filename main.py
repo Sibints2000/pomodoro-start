dc0b299aae3f7c00d6d93943eb71e92334276d8b
Sibints2000 <sibints2000@gmail.com>
2021-08-24 18:48:04 +0530

    Forty Ninth Commit

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    print(count)
+    canvas.itemconfig(timer_text, text=count)
     if count > 0:
         window.after(1000, count_down, count - 1)
 
@@ -32,7 +32,7 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 start_button = Button(text="Start", highlightthickness=0)
